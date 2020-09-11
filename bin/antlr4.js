83301fba8d05bbe1e861ff1d5b3d1c1b0d87eb9f
Alejandro Romero Herrera <alromh87@gmail.com>
2020-09-12 02:57:07 +0300

    Use execFile instead of shelljs
    Revert "Back to shelljs but sanitize args"
    
    This reverts commit 3ae86685b6de83994d03bb76d09c90e151201a66.

--- bin/antlr4.js
+++ bin/antlr4.js
@@ -1,12 +1,6 @@
 #! /usr/bin/env node
 
-var shell = require('shelljs');
+var execFile = require('child_process').execFile;
 
 var arguments = process.argv.splice(2);
-
-for (i=0;i<arguments.length; i++){
-  arguments[i] = arguments[i].replace(/'|"|;|&|,|\\|\|/gi, '')
-}
-
-var arguments = ' ' + arguments.join(' ');
-shell.exec('java -jar ' + __dirname + '/antlr-4.5-complete.jar' + arguments);
+execFile('java', ['-jar', __dirname, '/antlr-4.5-complete.jar'].concat(arguments));
