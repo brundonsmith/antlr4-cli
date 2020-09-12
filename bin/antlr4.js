0a7255a5b9ea46dfd38c03be8d403658f7077f2f
Alejandro Romero Herrera <alromh87@gmail.com>
2020-09-12 03:07:54 +0300

    Print output and remove shelljs dependency, fix typo

--- bin/antlr4.js
+++ bin/antlr4.js
@@ -3,4 +3,9 @@
 var execFile = require('child_process').execFile;
 
 var arguments = process.argv.splice(2);
-execFile('java', ['-jar', __dirname, '/antlr-4.5-complete.jar'].concat(arguments));
+execFile('java', ['-jar', __dirname+'/antlr-4.5-complete.jar'].concat(arguments), (error, stdout, stderr) => {
+  if (error) {
+    throw error;
+  }
+  console.log(stdout);
+});
